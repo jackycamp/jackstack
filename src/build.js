25d6a47247adca9e90bdcb338a0aee7170005e1a
jackycamp <jackcampanella08@gmail.com>
2023-07-17 14:04:17 -0400

    migrating to md to html and build tools

---
+++ src/build.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+const {marked} = require('marked')
+
+console.log("..starting build");
+
+const distDir = path.resolve(__dirname, '..', 'dist');
+const outPagesDir = path.resolve(distDir, 'pages');
+if (fs.existsSync(distDir)) {
+    console.log("cleaning up cached dist directory");
+    fs.rmSync(distDir, {recursive: true});
+}
+
+fs.mkdirSync(distDir);
+fs.mkdirSync(outPagesDir);
+
+const pagesDir = path.resolve(__dirname, 'pages');
+const files = fs.readdirSync(pagesDir);
+
+files.forEach((fileName) => {
+    const filePath = path.resolve(pagesDir, fileName);
+    const fileData = fs.readFileSync(filePath, 'utf8');
+    const html = marked.parse(fileData);
+
+    let htmlFileName = fileName.split(".md")[0];
+    htmlFileName += ".html";
+    console.log("htmlFileName", htmlFileName);
+
+    const outPath = path.resolve(distDir, 'pages', htmlFileName);
+    fs.writeFileSync(outPath, html);
+});
---
+++ src/pages/eks-getting-started.md
@@ -0,0 +1,8 @@
+
+# This is a title
+
+this is some random text
+
+```bash
+this is a code block lol
+```
