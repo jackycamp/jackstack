1a46cb57a38a0c8838421586dda8c9b8366ad9f4
jackycamp <jackcampanella08@gmail.com>
2023-07-19 18:01:36 -0400

    wip for books section

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,8 @@ const systemsPages = [
     { title: "Noob's guide to EKS", file: 'eks-getting-started.html'}
 ];
 
+const bookPages = [];
+
 const handleSystemsClick = () => {
     let contentNode = document.getElementById("content");
     contentNode.innerHTML = '';
@@ -26,15 +28,47 @@ const handleSystemsClick = () => {
         link.textContent = page.title;
         list.appendChild(link);
     });
-
-    
 }
+
+const handleBooksClick = () => {
+    let contentNode = document.getElementById("content");
+    contentNode.innerHTML = '';
+
+    let heading = document.createElement('h1');
+    heading.textContent = 'books';
+
+    contentNode.appendChild(heading);
+
+    let subheading = document.createElement('h3');
+    let subheadingText = "sometimes i collect notes and summaries from what i've read and compile them "
+    subheadingText += "as an auxiliary mechanism for my own capture and retention purposes."
+    subheading.textContent = subheadingText;
+
+    contentNode.appendChild(subheading);
+
+    let list = document.createElement('div');
+    list.classList = ['scrollable link_list'];
+    contentNode.appendChild(list);
+    
+    bookPages.forEach((page) => {
+        let link = document.createElement('a');
+        link.href = `pages/${page.file}`;
+        link.textContent = page.title;
+        list.appendChild(link);
+    });
+
+};
 
 
 window.onload = () => {
     let sysLink = document.getElementById('systems-link');
     if(sysLink) {
         sysLink.addEventListener('click', handleSystemsClick);
+    }
+
+    let booksLink = document.getElementById('books-link');
+    if (booksLink) {
+        booksLink.addEventListener('click', handleBooksClick)
     }
 }
 
