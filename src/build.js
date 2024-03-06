260752894bad5348b63870fa2d5b78c77a4de9af
jackycamp <jackcampanella08@gmail.com>
2024-03-05 21:42:40 -0500

    adding about page to build step

--- src/build.js
+++ src/build.js
@@ -1,111 +1,121 @@
-const fs = require('fs');
-const path = require('path');
-const {marked} = require('marked')
-const utils = require('./utils');
+const fs = require("fs");
+const path = require("path");
+const { marked } = require("marked");
+const utils = require("./utils");
 
 marked.setOptions({ mangle: false, headerIds: false });
 
 console.log("..starting build");
 
 // preparing dist/ directory
-const distDir = path.resolve(__dirname, '..', 'dist');
-const outPagesDir = path.resolve(distDir, 'pages');
-const outStylesDir = path.resolve(distDir, 'styles');
+const distDir = path.resolve(__dirname, "..", "dist");
+const outPagesDir = path.resolve(distDir, "pages");
+const outStylesDir = path.resolve(distDir, "styles");
 
 if (fs.existsSync(distDir)) {
-    console.log("cleaning up cached dist directory");
-    fs.rmSync(distDir, {recursive: true});
+  console.log("cleaning up cached dist directory");
+  fs.rmSync(distDir, { recursive: true });
 }
 
 fs.mkdirSync(distDir);
 fs.mkdirSync(outPagesDir);
 fs.mkdirSync(outStylesDir);
 
-
 // filling dist/ with stuff from src/
 // generating pages, stylesheets, etc.
-const pagesDir = path.resolve(__dirname, 'pages');
+const pagesDir = path.resolve(__dirname, "pages");
 const files = fs.readdirSync(pagesDir);
 
 const systemsPages = [];
 const booksPages = [];
 const codestuffPages = [];
 
-console.log("building pages")
+console.log("building pages");
 
 files.forEach((fileName) => {
-    const filePath = path.resolve(pagesDir, fileName);
-    const fileData = fs.readFileSync(filePath, 'utf8');
-    const lines = fileData.split("\n");
+  const filePath = path.resolve(pagesDir, fileName);
+  const fileData = fs.readFileSync(filePath, "utf8");
+  const lines = fileData.split("\n");
 
-    // meta portion of the file
-    const metaLines = lines.slice(0,3);
-    const label = metaLines.find((m) => m.startsWith("label")).split(":")[1];
-    const name = metaLines.find((m) => m.startsWith("name")).split(":")[1];
+  // meta portion of the file
+  const metaLines = lines.slice(0, 3);
+  const label = metaLines.find((m) => m.startsWith("label")).split(":")[1];
+  const name = metaLines.find((m) => m.startsWith("name")).split(":")[1];
 
-    // actual content that is converted to html
-    const content = lines.slice(3).join("\n");
-    const body = marked.parse(content);
-    const html = utils.createPage(body);
+  // actual content that is converted to html
+  const content = lines.slice(3).join("\n");
+  const body = marked.parse(content);
+  const html = utils.createPage(body);
 
-    // create the html file
-    let htmlFileName = fileName.split(".md")[0];
-    htmlFileName += ".html";
-    console.log("htmlFileName", htmlFileName);
-    const outPath = path.resolve(distDir, 'pages', htmlFileName);
-    fs.writeFileSync(outPath, html);
+  // create the html file
+  let htmlFileName = fileName.split(".md")[0];
+  htmlFileName += ".html";
+  console.log("htmlFileName", htmlFileName);
+  const outPath = path.resolve(distDir, "pages", htmlFileName);
+  fs.writeFileSync(outPath, html);
 
-    const fileMeta = { name, file: htmlFileName };
+  const fileMeta = { name, file: htmlFileName };
 
-    // classify the page
-    if (label === 'systems') systemsPages.push(fileMeta);
-    if (label === 'books') booksPages.push(fileMeta);
-    if (label === 'codestuff') codestuffPages.push(fileMeta);
+  // classify the page
+  if (label === "systems") systemsPages.push(fileMeta);
+  if (label === "books") booksPages.push(fileMeta);
+  if (label === "codestuff") codestuffPages.push(fileMeta);
 });
-
 
 console.log("generating stylesheets");
 // just copies over stylesheets, nothing fancy
-const stylesDir = path.resolve(__dirname, 'styles');
+const stylesDir = path.resolve(__dirname, "styles");
 const styleFiles = fs.readdirSync(stylesDir);
 
 styleFiles.forEach((fileName) => {
-    const srcPath = path.resolve(stylesDir, fileName);
-    const dstPath = path.resolve(outStylesDir, fileName);
+  const srcPath = path.resolve(stylesDir, fileName);
+  const dstPath = path.resolve(outStylesDir, fileName);
 
-    fs.copyFileSync(srcPath, dstPath);
+  fs.copyFileSync(srcPath, dstPath);
 });
 
 console.log("generating js");
 // copy over the js
-const jsDir = path.resolve(__dirname, 'js');
+const jsDir = path.resolve(__dirname, "js");
 const jsFiles = fs.readdirSync(jsDir);
 
 jsFiles.forEach((fileName) => {
-    const srcPath = path.resolve(jsDir, fileName);
-    const dstPath = path.resolve(distDir, fileName);
+  const srcPath = path.resolve(jsDir, fileName);
+  const dstPath = path.resolve(distDir, fileName);
 
-    fs.copyFileSync(srcPath, dstPath);
+  fs.copyFileSync(srcPath, dstPath);
 
-    // for the index.js file, need to inject the category arrays
-    // into the distributed version.
-    if (fileName === 'index.js') {
-        let indexJsData = fs.readFileSync(dstPath, 'utf8');
-        indexJsData = indexJsData.replace('__SYSTEMS_PAGES__', JSON.stringify(systemsPages));
-        indexJsData = indexJsData.replace('__BOOKS_PAGES__', JSON.stringify(booksPages));
-        indexJsData = indexJsData.replace('__CODESTUFF_PAGES__', JSON.stringify(codestuffPages));
-        fs.writeFileSync(dstPath, indexJsData);
-    }
+  // for the index.js file, need to inject the category arrays
+  // into the distributed version.
+  if (fileName === "index.js") {
+    let indexJsData = fs.readFileSync(dstPath, "utf8");
+    indexJsData = indexJsData.replace(
+      "__SYSTEMS_PAGES__",
+      JSON.stringify(systemsPages),
+    );
+    indexJsData = indexJsData.replace(
+      "__BOOKS_PAGES__",
+      JSON.stringify(booksPages),
+    );
+    indexJsData = indexJsData.replace(
+      "__CODESTUFF_PAGES__",
+      JSON.stringify(codestuffPages),
+    );
+    fs.writeFileSync(dstPath, indexJsData);
+  }
 });
 
 // generate entrypoint (dist/index.html)
-const indexOutPath = path.resolve(distDir, 'index.html');
+const indexOutPath = path.resolve(distDir, "index.html");
 const indexHtml = utils.createIndexPage();
 fs.writeFileSync(indexOutPath, indexHtml);
 
-const _404OutPath = path.resolve(distDir, '404.html');
+const aboutPath = path.resolve(distDir, "about.html");
+const aboutHtml = utils.createAboutPage();
+fs.writeFileSync(aboutPath, aboutHtml);
+
+const _404OutPath = path.resolve(distDir, "404.html");
 const _404Html = utils.get404Html();
 fs.writeFileSync(_404OutPath, _404Html);
 
 console.log("build done");
-
